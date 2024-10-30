cce644d0619c478e65c6a59e37dc559e771b7e18
EricJDarnell <eric.j.darnell@gmail.com>
2024-10-30 00:51:15 -0400

    puppy id on click

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -3,16 +3,20 @@ import { puppyList } from "./data";
 import "./App.css";
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log('puppies: ', puppies);
- 
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log("puppies: ", puppies);
+
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })
-      }
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      { featPupId && <p>{featPupId}</p>}
     </div>
   );
 }
