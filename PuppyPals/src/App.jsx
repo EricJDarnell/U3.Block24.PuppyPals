3011eecfb6e11df8c546c85864d19bc100106a2a
EricJDarnell <eric.j.darnell@gmail.com>
2024-10-30 10:32:58 -0400

    a bit of css and ready to publish

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -6,10 +6,12 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log('featuredPup: ', featuredPup);
   console.log("puppies: ", puppies);
 
   return (
     <div className="App">
+      <h1>Pupps On Parade!!</h1>
       {puppies.map((puppy) => {
         return (
           <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
